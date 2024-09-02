3daad249a22b69ccce6d1c48efd5b358941ffa1f
yena <50291995+nyj001012@users.noreply.github.com>
2024-09-02 15:21:11 +0900

    feat: app module에 histories repository 추가

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,10 +3,11 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { HistoriesController } from './controller/histories.controller';
 import { HistoriesService } from './service/histories.service';
+import { HistoriesRepository } from './repository/histories.repository';
 
 @Module({
   imports: [],
   controllers: [AppController, HistoriesController],
-  providers: [AppService, HistoriesService],
+  providers: [AppService, HistoriesService, HistoriesRepository],
 })
 export class AppModule {}
