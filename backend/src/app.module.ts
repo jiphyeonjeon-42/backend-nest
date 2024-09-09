a2deb953c702c84174209257f9c78f53f62f3631
yena <50291995+nyj001012@users.noreply.github.com>
2024-09-09 17:21:42 +0900

    feat: :sparkles: app module에 데이터베이스 연결하는 소스 추가

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,13 +1,27 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm'; // Add this line
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { HistoriesController } from './controller/histories.controller';
-import { HistoriesService } from './service/histories.service';
-import { HistoriesRepository } from './repository/histories.repository';
+import { HistoriesModule } from './repository/module/histories.module';
+
+import * as dotenv from 'dotenv';
+dotenv.config();
 
 @Module({
-  imports: [],
-  controllers: [AppController, HistoriesController],
-  providers: [AppService, HistoriesService, HistoriesRepository],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: process.env.RDS_HOSTNAME,
+      port: 3306,
+      username: process.env.RDS_USERNAME,
+      password: process.env.RDS_PASSWORD,
+      database: process.env.RDS_DB_NAME,
+      entities: [__dirname + '/**/*{.ts,.js}'],
+      synchronize: false,
+    }),
+    HistoriesModule,
+  ],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
