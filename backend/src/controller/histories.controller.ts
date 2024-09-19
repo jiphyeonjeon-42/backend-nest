7cffc3dcbebe771d35726726497525a787d42314
yena <50291995+nyj001012@users.noreply.github.com>
2024-09-19 15:43:46 +0900

    feat: histories swagger api 구조 작성

--- backend/src/controller/histories.controller.ts
+++ backend/src/controller/histories.controller.ts
@@ -1,4 +1,5 @@
 import { Controller, Get } from '@nestjs/common';
+import { ApiOperation, ApiParam, ApiResponse } from '@nestjs/swagger';
 import { HistoriesService } from 'src/service/histories.service';
 
 @Controller('histories')
@@ -6,6 +7,12 @@ export class HistoriesController {
   constructor(private historiesService: HistoriesService) {}
 
   @Get()
+  @ApiOperation({
+    summary: '모든 히스토리를 반환합니다.',
+    description: '모든 히스토리를 반환합니다.',
+  })
+  @ApiParam({ name: 'name', description: '이름', example: 'Nest' })
+  @ApiResponse({ status: 200, description: '성공', example: 'Hello Nest' })
   getHistories() {
     return this.historiesService.getHistories();
   }
