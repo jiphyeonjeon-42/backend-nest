0225bcb6c97d8976c05cde07259e6edf6143712c
scarf <greenscarf005@gmail.com>
2024-09-19 23:40:52 +0900

    feat: GET `/lendings`

--- backend/src/lendings/lendings.controller.ts
+++ backend/src/lendings/lendings.controller.ts
@@ -1,6 +1,7 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post, Query } from '@nestjs/common';
 import {
   ApiCreatedResponse,
+  ApiOkResponse,
   ApiOperation,
   ApiTags,
 } from '@nestjs/swagger';
@@ -11,7 +12,9 @@ import {
   bookIdSchema,
   userIdSchema,
   intSchema,
-} from 'src/dto';
+  limitSchema,
+  positiveSchema,
+} from 'src/dto'
 
 const lendingIdSchema = intSchema.describe('대출 기록 고유 ID');
 const conditionSchema = extendApi(z.string().describe('도서 상태'), {
@@ -32,6 +35,25 @@ class CreateLendingsResponseDto extends createZodDto(
   createLendingsResponseSchema,
 ) {}
 
+const searchLendingsSchema = z.object({
+  page: positiveSchema.default(1).optional().describe('검색 결과의 페이지'),
+  limit: limitSchema.default(5).optional(),
+  sort: z
+    .enum(['new', 'old'])
+    .default('new')
+    .optional()
+    .describe('검색 결과를 정렬할 기준'),
+  query: extendApi(
+    z.string().optional().describe('대출 기록에서 검색할 단어'),
+    { example: '파이썬' },
+  ),
+  type: z
+    .enum(['user', 'title', 'callSign', 'bookId'])
+    .optional()
+    .describe('query로 조회할 항목'),
+});
+class SearchLendingsDto extends createZodDto(searchLendingsSchema) {}
+
 @Controller('lendings')
 @ApiTags('lendings')
 export class LendingsController {
@@ -51,4 +73,20 @@ export class LendingsController {
   ): Promise<CreateLendingsResponseDto> {
     return { dueDate: new Date() };
   }
+
+  @ApiOperation({ deprecated: true, summary: '대출 기록 정보 조회' })
+  @Get('search')
+  searchLendingsLegacy(@Query() query: SearchLendingsDto) {
+    return this.searchLendings(query);
+  }
+
+  @Get()
+  @ApiOperation({
+    summary: '대출 기록 정보 조회',
+    description: '대출 기록의 정보를 검색하여 보여줍니다.',
+  })
+  @ApiOkResponse({
+    description: '대출 기록 정보를 반환합니다.',
+  })
+  searchLendings(@Query() query: SearchLendingsDto) {}
 }
