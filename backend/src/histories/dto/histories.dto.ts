aec102d1d5f3bf9e3ed1e1a6ffe85065ff1863e0
yena <50291995+nyj001012@users.noreply.github.com>
2024-09-20 00:45:24 +0900

    Update backend/src/histories/dto/histories.dto.ts
    Co-authored-by: scarf <greenscarf005@gmail.com>

--- backend/src/histories/dto/histories.dto.ts
+++ backend/src/histories/dto/histories.dto.ts
@@ -1,4 +1,4 @@
-import { createZodDto } from 'nestjs-zod';
+import { createZodDto } from '@anatine/zod-nestjs';
 import { getHistoriesRequestSchema } from '../schema/histories.schema';
 
 export class getHistoriesDto extends createZodDto(getHistoriesRequestSchema) {}
