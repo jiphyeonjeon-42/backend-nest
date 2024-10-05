bce64b770c091852eefd6d46ade40aa06914008d
scarf <greenscarf005@gmail.com>
2024-10-05 17:59:09 +0900

    refactor: TypeOrmModule로 통일
    참고: https://docs.nestjs.com/techniques/database

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,24 +3,10 @@ import { TypeOrmModule } from '@nestjs/typeorm'; // Add this line
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { HistoriesModule } from './histories/histories.module';
-
-import * as dotenv from 'dotenv';
-dotenv.config();
+import { dbConfig } from './config';
 
 @Module({
-  imports: [
-    TypeOrmModule.forRoot({
-      type: 'mysql',
-      host: process.env.RDS_HOSTNAME,
-      port: 3306,
-      username: process.env.RDS_USERNAME,
-      password: process.env.RDS_PASSWORD,
-      database: process.env.RDS_DB_NAME,
-      entities: [__dirname + '/**/*{.ts,.js}'],
-      synchronize: false,
-    }),
-    HistoriesModule,
-  ],
+  imports: [TypeOrmModule.forRoot(dbConfig), HistoriesModule],
   controllers: [AppController],
   providers: [AppService],
 })
